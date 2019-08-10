8b8777513ac8c9c6ce1b4a6b3f9fc871ea6d4547
Naty89 <37753984+Naty89@users.noreply.github.com>
2019-08-10 13:46:31 -0400

    Update chunkify.py

--- chunkify.py
+++ chunkify.py
@@ -1,9 +1,7 @@
 def chunkify(x, y):
-	# p = []
-	# for i in range(0, len(x), y):
-	# 	p.append(x[i:i + y])
-	# return p
-
-	return [x[i:i + y] for i in range(0, len(x), y)]
+	p = []
+	for i in range(0, len(x), y):
+		p.append(x[i:i + y])
+	return p
 
 print(chunkify([1,2,3,4,5,6], 2))
